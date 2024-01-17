1b86249a0bef6885c23a249d36105cadf2ff809d
Travis Wagner <travis.wagner@umusic.com>
2024-01-17 17:17:00 -0500

    refactor build and serve script in start.ts

--- scripts/start.ts
+++ scripts/start.ts
@@ -6,16 +6,11 @@ const shouldServe = !process.argv.includes("--no-server");
 const thisDir = path.dirname(new URL(import.meta.url).pathname);
 const spawnOpts = { stdout: "pipe", stderr: "pipe" } as const;
 const build = Bun.spawn(["bun", "--watch", path.join(thisDir, "build.ts"), "-w"], spawnOpts);
-
-if (!shouldServe) {
-    printStream(build.stdout);
-    printStream(build.stderr);
-}
+printStream(build.stdout, "build");
+printStream(build.stderr, "build");
 
 if (shouldServe) {
     const serve = Bun.spawn(["bun", "--hot", path.join(thisDir, "serve.ts")], spawnOpts);
     printStream(serve.stdout, "serve");
     printStream(serve.stderr, "serve");
-    printStream(build.stdout, "build");
-    printStream(build.stderr, "build");
 }
